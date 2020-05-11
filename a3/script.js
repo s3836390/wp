b8a7d2951151cde635ef06912707f8d4c7b2ef33
s3836390 <s3836390@rmit.edu.vn>
2020-05-11 23:48:58 +0700

    assign3

--- a3/script.js
+++ a3/script.js
@@ -1,51 +1,62 @@
-function current1(){
-document.getElementById("link1").style.color="red";
-document.getElementById("link2").style.color=("blue");
-document.getElementById("link3").style.color=("blue");
+function current1() {
+  document.getElementById("link1").style.color = "red";
+  document.getElementById("link2").style.color = ("blue");
+  document.getElementById("link3").style.color = ("blue");
 }
-function current2(){
-document.getElementById("link2").style.color="red";
-document.getElementById("link1").style.color=("blue");
-document.getElementById("link3").style.color=("blue");
+function current2() {
+  document.getElementById("link2").style.color = "red";
+  document.getElementById("link1").style.color = ("blue");
+  document.getElementById("link3").style.color = ("blue");
 }
-function current3(){
-document.getElementById("link3").style.color="red";
-document.getElementById("link1").style.color=("blue");
-document.getElementById("link2").style.color=("blue");
- }
-document.getElementById("wheel1").addEventListener("wheel",current1)
-document.getElementById("wheel2").addEventListener("wheel",current2)
-document.getElementById("wheel3").addEventListener("wheel",current3)
+function current3() {
+  document.getElementById("link3").style.color = "red";
+  document.getElementById("link1").style.color = ("blue");
+  document.getElementById("link2").style.color = ("blue");
+}
+document.getElementById("wheel1").addEventListener("wheel", current1)
+document.getElementById("wheel2").addEventListener("wheel", current2)
+document.getElementById("wheel3").addEventListener("wheel", current3)
 
-function showhidden1(){
-  document.getElementById("synopsisACT").style.visibility="visible"
-  document.getElementById("synopsisRMC").style.visibility="hidden"
-  document.getElementById("synopsisANM").style.visibility="hidden"
-  document.getElementById("synopsisAHF").style.visibility="hidden"
+function showhidden1() {
+  document.getElementById("synopsisACT").style.visibility = "visible"
+  document.getElementById("synopsisRMC").style.visibility = "hidden"
+  document.getElementById("synopsisANM").style.visibility = "hidden"
+  document.getElementById("synopsisAHF").style.visibility = "hidden"
 }
-function showhidden2(){
-  document.getElementById("synopsisRMC").style.visibility="visible"
-  document.getElementById("synopsisACT").style.visibility="hidden"
-  document.getElementById("synopsisANM").style.visibility="hidden"
-  document.getElementById("synopsisAHF").style.visibility="hidden"
+function showhidden2() {
+  document.getElementById("synopsisRMC").style.visibility = "visible"
+  document.getElementById("synopsisACT").style.visibility = "hidden"
+  document.getElementById("synopsisANM").style.visibility = "hidden"
+  document.getElementById("synopsisAHF").style.visibility = "hidden"
 }
-function showhidden3(){
-  document.getElementById("synopsisANM").style.visibility="visible"
-  document.getElementById("synopsisACT").style.visibility="hidden"
-  document.getElementById("synopsisRMC").style.visibility="hidden"
-  document.getElementById("synopsisAHF").style.visibility="hidden"
+function showhidden3() {
+  document.getElementById("synopsisANM").style.visibility = "visible"
+  document.getElementById("synopsisACT").style.visibility = "hidden"
+  document.getElementById("synopsisRMC").style.visibility = "hidden"
+  document.getElementById("synopsisAHF").style.visibility = "hidden"
 }
-function showhidden4(){
-  document.getElementById("synopsisAHF").style.visibility="visible"
-  document.getElementById("synopsisACT").style.visibility="hidden"
-  document.getElementById("synopsisRMC").style.visibility="hidden"
-  document.getElementById("synopsisANM").style.visibility="hidden"
+function showhidden4() {
+  document.getElementById("synopsisAHF").style.visibility = "visible"
+  document.getElementById("synopsisACT").style.visibility = "hidden"
+  document.getElementById("synopsisRMC").style.visibility = "hidden"
+  document.getElementById("synopsisANM").style.visibility = "hidden"
 }
-var movieid = {
-  md1:ACT,
-}
-var movieday ="MON,TUE,WED,THU,FRI,SAT,SUN";
-var moviehour ="T12,T15,T18,T21";
-var prices = {   p1: { o1:18.5, o2:15.5, o3:30 },  p2: { o1:28.5, o2:25.5, o3:40 },  p3: { o1:38.5, o2:35.5, o3:50 }};
-alert(prices.p2.o3); 
-alert(prices.p3.o1);
+
+var today = new Date();
+var chosendate = document.getElementById("cust-expiry") ;
+function futureonly() {
+if (chosendate>today) {
+  alert("Please insert future date");
+}}
+
+var seatsSTA = document.getElementById('seats-STA').value;
+var seatsSTP = document.getElementById('seats-STP').value;
+var seatsSTC = document.getElementById('seats-STC').value;
+var seatsSTA = document.getElementById('seats-STA').value;
+var seatsSTP = document.getElementById('seats-STP').value;
+var seatsSTC = document.getElementById('seats-STC').value;
+
+
+
+document.getElementById('totalprice').innerHTML = (seatsSTA*3).toFixed(2);
+
