7ae25847117aa993ed7cd69232c48bb8caf44b26
s3836390 <s3836390@rmit.edu.vn>
2020-05-11 14:19:40 +0700

    das

--- a3/script.js
+++ a3/script.js
@@ -1,3 +1,15 @@
-function currentlink() {
-    document.getElementsByClassName(currentlink)
+function current1(){
+document.getElementById("link1").style.color="red";
+document.getElementById("link2").style.color=("blue");
+document.getElementById("link3").style.color=("blue");
 }
+function current2(){
+document.getElementById("link2").style.color="red";
+document.getElementById("link1").style.color=("blue");
+document.getElementById("link3").style.color=("blue");
+}
+function current3(){
+document.getElementById("link3").style.color="red";
+document.getElementById("link1").style.color=("blue");
+document.getElementById("link2").style.color=("blue");
+ }
