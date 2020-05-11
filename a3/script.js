e3059011104040d280ee97dca51a7c2f7a28f046
s3836390 <s3836390@rmit.edu.vn>
2020-05-11 15:21:10 +0700

    fad

--- a3/script.js
+++ a3/script.js
@@ -13,3 +13,6 @@ document.getElementById("link3").style.color="red";
 document.getElementById("link1").style.color=("blue");
 document.getElementById("link2").style.color=("blue");
  }
+document.getElementById("wheel1").addEventListener("wheel",current1)
+document.getElementById("wheel2").addEventListener("wheel",current2)
+document.getElementById("wheel3").addEventListener("wheel",current3)
