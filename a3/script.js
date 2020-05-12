66a00ad90d2c65a9407fc4d21bc986a8d3dece83
s3836390 <s3836390@rmit.edu.vn>
2020-05-12 17:33:41 +0700

    dasd

--- a3/script.js
+++ a3/script.js
@@ -67,7 +67,7 @@ function booking1(){
   document.getElementById('movie-hour').value='T21';
   document.getElementById('title').innerHTML='Avengers: Endgame';
   document.getElementById('day').innerHTML='Wednesday';
-  document.getElementById('time').innerHTML='21pm';
+  document.getElementById('time').innerHTML='9pm';
   priceSTA.value=14;
   priceSTP.value=12.5;
   priceSTC.value=11;
@@ -83,7 +83,7 @@ function booking2(){
   document.getElementById('movie-hour').value='T21';
   document.getElementById('title').innerHTML='Avengers: Endgame';
   document.getElementById('day').innerHTML='Thursday';
-  document.getElementById('time').innerHTML='21pm';
+  document.getElementById('time').innerHTML='9pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -99,7 +99,7 @@ function booking3(){
   document.getElementById('movie-hour').value='T21';
   document.getElementById('title').innerHTML='Avengers: Endgame';
   document.getElementById('day').innerHTML='Friday';
-  document.getElementById('time').innerHTML='21pm';
+  document.getElementById('time').innerHTML='9pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -115,7 +115,7 @@ function booking4(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Avengers: Endgame';
   document.getElementById('day').innerHTML='Saturday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -131,7 +131,7 @@ function booking5(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Avengers: Endgame';
   document.getElementById('day').innerHTML='Sunday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -147,7 +147,7 @@ function booking6(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Top End Wedding';
   document.getElementById('day').innerHTML='Monday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=14;
   priceSTP.value=12.5;
   priceSTC.value=11;
@@ -163,7 +163,7 @@ function booking7(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Top End Wedding';
   document.getElementById('day').innerHTML='Tuesday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -179,7 +179,7 @@ function booking8(){
   document.getElementById('movie-hour').value='T15';
   document.getElementById('title').innerHTML='Top End Wedding';
   document.getElementById('day').innerHTML='Saturday';
-  document.getElementById('time').innerHTML='15pm';
+  document.getElementById('time').innerHTML='3pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -195,7 +195,7 @@ function booking9(){
   document.getElementById('movie-hour').value='T15';
   document.getElementById('title').innerHTML='Top End Wedding';
   document.getElementById('day').innerHTML='Sunday';
-  document.getElementById('time').innerHTML='15pm';
+  document.getElementById('time').innerHTML='3pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -243,7 +243,7 @@ function booking12(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Dumbo';
   document.getElementById('day').innerHTML='Wednesday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=14;
   priceSTP.value=12.5;
   priceSTC.value=11;
@@ -259,7 +259,7 @@ function booking13(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Dumbo';
   document.getElementById('day').innerHTML='Thursday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -275,7 +275,7 @@ function booking14(){
   document.getElementById('movie-hour').value='T18';
   document.getElementById('title').innerHTML='Dumbo';
   document.getElementById('day').innerHTML='Friday';
-  document.getElementById('time').innerHTML='18pm';
+  document.getElementById('time').innerHTML='6pm';
   priceSTA.value=19.8;
   priceSTP.value=17.5;
   priceSTC.value=15.3;
@@ -332,6 +332,69 @@ function booking17(){
   priceFTC.value=21;
 }
 
+function booking18(){
+  
+  document.getElementById('movie-id').value='AHF';
+  document.getElementById('movie-day').value='THU';
+  document.getElementById('movie-hour').value='T12';
+  document.getElementById('title').innerHTML='The Happy Prince';
+  document.getElementById('day').innerHTML='Thursday';
+  document.getElementById('time').innerHTML='12pm';
+  priceSTA.value=14;
+  priceSTP.value=12.5;
+  priceSTC.value=11;
+  priceFTA.value=24;
+  priceFTP.value=22.5;
+  priceFTC.value=21;
+}
+
+function booking19(){
+  
+  document.getElementById('movie-id').value='AHF';
+  document.getElementById('movie-day').value='FRI';
+  document.getElementById('movie-hour').value='T12';
+  document.getElementById('title').innerHTML='The Happy Prince';
+  document.getElementById('day').innerHTML='Friday';
+  document.getElementById('time').innerHTML='12pm';
+  priceSTA.value=14;
+  priceSTP.value=12.5;
+  priceSTC.value=11;
+  priceFTA.value=24;
+  priceFTP.value=22.5;
+  priceFTC.value=21;
+}
+
+function booking20(){
+  
+  document.getElementById('movie-id').value='AHF';
+  document.getElementById('movie-day').value='SAT';
+  document.getElementById('movie-hour').value='T21';
+  document.getElementById('title').innerHTML='The Happy Prince';
+  document.getElementById('day').innerHTML='Saturday';
+  document.getElementById('time').innerHTML='9pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking21(){
+  
+  document.getElementById('movie-id').value='AHF';
+  document.getElementById('movie-day').value='SUN';
+  document.getElementById('movie-hour').value='T21';
+  document.getElementById('title').innerHTML='The Happy Prince';
+  document.getElementById('day').innerHTML='Sunday';
+  document.getElementById('time').innerHTML='9pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
 
 function calculate(){
   var seatsSTA = document.getElementById('seats-STA').value;
