2a1bbf1ef178f8ca5d1088fec49039d774dcdcee
s3836390 <s3836390@rmit.edu.vn>
2020-05-12 17:00:50 +0700

    dasd

--- a3/script.js
+++ a3/script.js
@@ -53,22 +53,167 @@ function futureonly() {
   }
 }
 
-function booking(){
-  document.getElementsByClassName('THU T21').value=19.8;
+var priceSTA={};
+var priceSTP={};
+var priceSTC={};
+var priceFTA={};
+var priceFTP={};
+var priceFTC={};
+
+function booking1(){
+  
+  document.getElementById('movie-id').value='ACT';
+  document.getElementById('movie-day').value='WED';
+  document.getElementById('movie-hour').value='T21';
+  document.getElementById('title').innerHTML='Avengers: Endgame';
+  document.getElementById('day').innerHTML='Wednesday';
+  document.getElementById('time').innerHTML='21pm';
+  priceSTA.value=14;
+  priceSTP.value=12.5;
+  priceSTC.value=11;
+  priceFTA.value=24;
+  priceFTP.value=22.5;
+  priceFTC.value=21;
+}
+
+function booking2(){
+  
   document.getElementById('movie-id').value='ACT';
   document.getElementById('movie-day').value='THU';
   document.getElementById('movie-hour').value='T21';
-  document
-}yP
-function sta(){
+  document.getElementById('title').innerHTML='Avengers: Endgame';
+  document.getElementById('day').innerHTML='Thursday';
+  document.getElementById('time').innerHTML='21pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking3(){
+  
+  document.getElementById('movie-id').value='ACT';
+  document.getElementById('movie-day').value='FRI';
+  document.getElementById('movie-hour').value='T21';
+  document.getElementById('title').innerHTML='Avengers: Endgame';
+  document.getElementById('day').innerHTML='Friday';
+  document.getElementById('time').innerHTML='21pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking4(){
+  
+  document.getElementById('movie-id').value='ACT';
+  document.getElementById('movie-day').value='SAT';
+  document.getElementById('movie-hour').value='T18';
+  document.getElementById('title').innerHTML='Avengers: Endgame';
+  document.getElementById('day').innerHTML='Saturday';
+  document.getElementById('time').innerHTML='18pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking5(){
+  
+  document.getElementById('movie-id').value='ACT';
+  document.getElementById('movie-day').value='SUN';
+  document.getElementById('movie-hour').value='T18';
+  document.getElementById('title').innerHTML='Avengers: Endgame';
+  document.getElementById('day').innerHTML='Sunday';
+  document.getElementById('time').innerHTML='18pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking6(){
+  
+  document.getElementById('movie-id').value='RMC';
+  document.getElementById('movie-day').value='MON';
+  document.getElementById('movie-hour').value='T18';
+  document.getElementById('title').innerHTML='Top End Wedding';
+  document.getElementById('day').innerHTML='Monday';
+  document.getElementById('time').innerHTML='18pm';
+  priceSTA.value=14;
+  priceSTP.value=12.5;
+  priceSTC.value=11;
+  priceFTA.value=24;
+  priceFTP.value=22.5;
+  priceFTC.value=21;
+}
+
+function booking7(){
+  
+  document.getElementById('movie-id').value='RMC';
+  document.getElementById('movie-day').value='TUE';
+  document.getElementById('movie-hour').value='T18';
+  document.getElementById('title').innerHTML='Top End Wedding';
+  document.getElementById('day').innerHTML='Tuesday';
+  document.getElementById('time').innerHTML='18pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking8(){
+  
+  document.getElementById('movie-id').value='RMC';
+  document.getElementById('movie-day').value='SAT';
+  document.getElementById('movie-hour').value='T15';
+  document.getElementById('title').innerHTML='Top End Wedding';
+  document.getElementById('day').innerHTML='Saturday';
+  document.getElementById('time').innerHTML='15pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+function booking9(){
+  
+  document.getElementById('movie-id').value='RMC';
+  document.getElementById('movie-day').value='SUN';
+  document.getElementById('movie-hour').value='T15';
+  document.getElementById('title').innerHTML='Top End Wedding';
+  document.getElementById('day').innerHTML='Sunday';
+  document.getElementById('time').innerHTML='15pm';
+  priceSTA.value=19.8;
+  priceSTP.value=17.5;
+  priceSTC.value=15.3;
+  priceFTA.value=30;
+  priceFTP.value=27;
+  priceFTC.value=24;
+}
+
+
+function calculate(){
   var seatsSTA = document.getElementById('seats-STA').value;
   var seatsSTP = document.getElementById('seats-STP').value;
   var seatsSTC = document.getElementById('seats-STC').value;
   var seatsFTA = document.getElementById('seats-FTA').value;
   var seatsFTP = document.getElementById('seats-FTP').value;
   var seatsFTC = document.getElementById('seats-FTC').value;
-  var priceSTA = document.getElementsByClassName('THU T21').value;
-  document.getElementById('totalprice').innerHTML = (seatsSTA*priceSTA).toFixed(2);
+ 
+  document.getElementById('totalprice').innerHTML = (seatsSTA*priceSTA.value + seatsSTP*priceSTP.value+ seatsSTC*priceSTC.value+seatsFTA*priceFTA.value+seatsFTP*priceFTP.value+seatsFTC*priceFTC.value).toFixed(2);
 }
 
 
